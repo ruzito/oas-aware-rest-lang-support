7e78054d8a40e41eaf2988d191bf1d6712b3936a
Tomas Ruzicka <truzicka@purestorage.com>
2025-01-16 12:05:05 +0100

    publish command and some test improvements

--- test_integration/tests/package.js
+++ test_integration/tests/package.js
@@ -3,11 +3,12 @@ import assert from "node:assert/strict";
 
 import * as fs from 'fs';
 
-const int_pkg_json = JSON.parse(fs.readFileSync('./package.json'));
+const int_pkg_json = JSON.parse(fs.readFileSync('../test_integration/package.json'));
 const bench_pkg_json = JSON.parse(fs.readFileSync('../test_benchmarks/package.json'));
 const be_pkg_json = JSON.parse(fs.readFileSync('../backend/package.json'));
+const fe_cm_pkg_json = JSON.parse(fs.readFileSync('../frontend_codemirror/package.json'));
 
-const pub_pkgs = [be_pkg_json]
+const pub_pkgs = [be_pkg_json, fe_cm_pkg_json]
 const priv_pkgs = [int_pkg_json, bench_pkg_json]
 
 const pkgs = [...pub_pkgs, ...priv_pkgs]
@@ -26,6 +27,13 @@ describe("package.json integrity", () => {
             assert.strictEqual(pkg["repository"]["type"], "git", `package "${pkg["name"]}" does not declare use a git repository`)
         }
     })
+    test("public packages should have set a description", () => {
+        for (let i in pub_pkgs) {
+            let pkg = pub_pkgs[i]
+            assert.strict(pkg["description"], `package "${pkg["name"]}" does not set a description`)
+            assert.strict(pkg["description"] !== "", `package "${pkg["name"]}" has set an empty description`)
+        }
+    })
     test("public packages should have set same git repository", () => {
         let repo = pub_pkgs[0]["repository"]["url"]
         for (let i in pub_pkgs) {
@@ -39,6 +47,13 @@ describe("package.json integrity", () => {
         for (let i in pub_pkgs) {
             let pkg = pub_pkgs[i]
             assert.strictEqual(pkg["version"], ver, `package "${pkg["name"]}" does not declare the same version as "${pub_pkgs[0]["name"]}"`)
+        }
+    })
+    test("public packages should have set same author (this is probably a mistake)", () => {
+        let author = pkgs[0]["author"]
+        for (let i in pkgs) {
+            let pkg = pkgs[i]
+            assert.strictEqual(pkg["author"], author, `package "${pkg["name"]}" does not declare the same author as "${pkgs[0]["name"]}"`)
         }
     })
     test("basic package json stuff", () => {
